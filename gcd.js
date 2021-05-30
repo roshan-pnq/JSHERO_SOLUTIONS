4dfc0b060890a2406ce33bd359723073ee9fa20e
= <=>
2021-05-30 17:13:42 +0530

    gcd of two numbers using while loop

---
+++ gcd.js
@@ -0,0 +1,11 @@
+function gcd(number1,number2){
+while(number1 != number2){
+    if(number1 > number2) {
+        number1 -= number2;
+    }
+    else {
+        number2 -= number1;
+    }
+}
+return number1
+}
